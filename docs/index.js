2ea8fae829ab1bcb0a422770b7326c14273782ea
abiel13 <dbestabi28@gmail.com>
2022-08-21 16:08:28 -0700

    shit

---
+++ docs/index.js
@@ -0,0 +1,18 @@
+function scroll() {
+    var cat = document.querySelectorAll('.cat');
+    cat.forEach(element => {
+        catPosition = element.getBoundingClientRect().top;
+        windowPos = window.innerHeight / 1.3;
+
+        if (catPosition < windowPos) {
+            element.classList.remove('fade-out')
+            element.classList.add('fade-in')
+        }
+        else {
+            element.classList.remove("fade-in")
+            element.classList.add("fade-out")
+        }
+    })
+}
+
+window.addEventListener('scroll', scroll)
